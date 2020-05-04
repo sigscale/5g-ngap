4a5f638adeb4c968cc96a99fc46fa0ab09a3f1f2
Vance Shipley <vances@sigscale.org>
2020-05-04 13:52:21 +0800

    autotools and application

---
+++ src/ngap_app.erl
@@ -0,0 +1,104 @@
+%%% ngap_app.erl
+%%% vim: ts=3
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @copyright 2020 SigScale Global Inc.
+%%% @end
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%    http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc This {@link //stdlib/application. application} behaviour callback
+%%% 	module starts and stops the {@link //ngap. ngap} application.
+%%%
+-module(ngap_app).
+-copyright('Copyright (c) 2020 SigScale Global Inc.').
+
+-behaviour(application).
+
+%% callbacks needed for application behaviour
+-export([start/2, stop/1, config_change/3]).
+%% optional callbacks for application behaviour
+-export([prep_stop/1, start_phase/3]).
+
+-record(state, {}).
+
+%%----------------------------------------------------------------------
+%% The ngap_app aplication callbacks
+%%----------------------------------------------------------------------
+
+-type start_type() :: normal | {takeover, node()} | {failover, node()}.
+-spec start(StartType, StartArgs) -> Result
+	when
+		StartType :: start_type(),
+		StartArgs :: term(),
+		Result :: {'ok', pid()} | {'ok', pid(), State} | {'error', Reason},
+		State :: #state{},
+		Reason :: term().
+%% @doc Starts the application processes.
+%% @see //kernel/application:start/1
+%% @see //kernel/application:start/2
+%%
+start(normal = _StartType, _Args) ->
+	supervisor:start_link(im_sup, []).
+
+%%----------------------------------------------------------------------
+%% The ngap_app private API
+%%----------------------------------------------------------------------
+
+-spec start_phase(Phase, StartType, PhaseArgs) -> Result
+	when
+		Phase :: atom(),
+		StartType :: start_type(),
+		PhaseArgs :: term(),
+		Result :: ok | {error, Reason},
+		Reason :: term().
+%% @doc Called for each start phase in the application and included
+%% 	applications.
+%% @see //kernel/app
+%%
+start_phase(_Phase, _StartType, _PhaseArgs) ->
+	ok.
+
+-spec prep_stop(State) -> #state{}
+	when
+		State :: #state{}.
+%% @doc Called when the application is about to be shut down,
+%% 	before any processes are terminated.
+%% @see //kernel/application:stop/1
+%%
+prep_stop(State) ->
+	State.
+
+-spec stop(State) -> any()
+	when
+		State :: #state{}.
+%% @doc Called after the application has stopped to clean up.
+%%
+stop(_State) ->
+	ok.
+
+-spec config_change(Changed, New, Removed) -> ok
+	when
+		Changed:: [{Par, Val}],
+		New :: [{Par, Val}],
+		Removed :: [Par],
+		Par :: atom(),
+		Val :: atom().
+%% @doc Called after a code replacement, if there are any
+%% 	changes to the configuration parameters.
+%%
+config_change(_Changed, _New, _Removed) ->
+	ok.
+
+%%----------------------------------------------------------------------
+%% internal functions
+%%----------------------------------------------------------------------
+
